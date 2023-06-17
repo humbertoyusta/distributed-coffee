ce11e4f69560a2c200b09fac94ac23957287f245
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-06-17 18:12:07 +0200

    feature/use-database-in-backend-app

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,31 @@
 from flask import Flask, Blueprint, request, jsonify
-from collections import Counter
-import random
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+import os
 import base64
 
 app = Flask(__name__)
 
+# Database configuration
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define a model for Users
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    coffee = db.Column(db.String(80), nullable=True)
+
 # Define a blueprint for version 1 of our API
 v1_blueprint = Blueprint('v1', __name__, url_prefix='/v1')
-
-# Define some sample users and coffees
-sample_users = ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy']
-sample_coffees = ['Espresso', 'Latte', 'Cappuccino', 'Americano']
-
-# Initialize in memory storage for users and their favourite coffees
-users = {user: random.choice(sample_coffees) for user in sample_users}
-coffee_counter = Counter(users.values())
 
 @v1_blueprint.route('/coffee/favourite', methods=['GET', 'POST'])
 def favourite_coffee():
@@ -23,7 +34,7 @@ def favourite_coffee():
     if not auth_header or not auth_header.startswith('Basic '):
         return jsonify({'error': 'Invalid or missing authorization'}), 401
     auth_string = base64.b64decode(auth_header[6:]).decode('utf-8')
-    user, password = auth_string.split(':')
+    user_name, password = auth_string.split(':')
 
     if request.method == 'POST':
         # Get favourite coffee from JSON body
@@ -31,24 +42,34 @@ def favourite_coffee():
         if not coffee:
             return jsonify({'error': 'Missing favouriteCoffee in JSON body'}), 400
 
-        # If user has already a favourite coffee, decrease its count
-        if user in users and users[user] in coffee_counter:
-            coffee_counter[users[user]] -= 1
+        # Find user in database or create a new one
+        user = User.query.filter_by(username=user_name).first()
+        if user is None:
+            user = User(username=user_name)
+            db.session.add(user)
 
-        # Update user's favourite coffee and increase its count
-        users[user] = coffee
-        coffee_counter[coffee] += 1
+        # Update user's favourite coffee
+        user.coffee = coffee
+        db.session.commit()
+
         return leaderboard()
 
     elif request.method == 'GET':
         # Return user's favourite coffee
-        return jsonify({'favouriteCoffee': users.get(user)})
-
+        user = User.query.filter_by(username=user_name).first()
+        if user is not None:
+            return jsonify({'favouriteCoffee': user.coffee})
+        else:
+            return jsonify({'error': 'User not found'}), 404
 
 @v1_blueprint.route('/admin/coffee/favourite/leadeboard', methods=['GET'])
 def leaderboard():
     # Show the top 3 most popular coffees
-    top_coffees = coffee_counter.most_common(3)
+    top_coffees = db.session.query(User.coffee, func.count(User.coffee))\
+                             .group_by(User.coffee)\
+                             .order_by(func.count(User.coffee).desc())\
+                             .limit(3)\
+                             .all()
     return jsonify({'top3': [{coffee: count} for coffee, count in top_coffees]})
 
 # Register our blueprint under the url_prefix /v1
@@ -59,4 +80,7 @@ def health():
     return jsonify({'status': 'healthy'})
 
 if __name__ == "__main__":
+    # Create database tables if they don't exist
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host="0.0.0.0", port=8080)
