bc8a46f2cb001ae6b2555c1d7ebf808a18ccdc34
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-06-22 00:39:50 +0200

    feature/add-create-user-that-send-to-aws-sqs

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 import os
 import base64
+import json
+import boto3
 
 app = Flask(__name__)
 
@@ -16,7 +18,10 @@ DATABASE_NAME = os.getenv('DATABASE_NAME')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) # Initialize SQLAlchemy
+sqs = boto3.client('sqs') # Initialize SQS client
+QUEUE_URL = os.getenv('QUEUE_URL')  # Get your SQS Queue URL from environment variable
+
 
 # Define a model for Users
 class User(db.Model):
@@ -24,8 +29,37 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     coffee = db.Column(db.String(80), nullable=True)
 
+
 # Define a blueprint for version 1 of our API
 v1_blueprint = Blueprint('v1', __name__, url_prefix='/v1')
+
+
+@v1_blueprint.route('/user', methods=['POST'])
+def create_user():
+    username = request.json.get('username')
+    if not username:
+        return jsonify({'error': 'Missing username in JSON body'}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if user is not None:
+        return jsonify({'error': 'User already exists'}), 400
+
+    user = User(username=username)
+    db.session.add(user)
+    db.session.commit()
+
+    # Send a message to SQS queue
+    sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps({
+            'userId': user.id,
+            'username': user.username,
+            'event': 'User Created'
+        })
+    )
+
+    return jsonify({'message': 'User created successfully'}), 201
+
 
 @v1_blueprint.route('/coffee/favourite', methods=['GET', 'POST'])
 def favourite_coffee():
@@ -36,17 +70,15 @@ def favourite_coffee():
     auth_string = base64.b64decode(auth_header[6:]).decode('utf-8')
     user_name, password = auth_string.split(':')
 
+    user = User.query.filter_by(username=user_name).first()
+    if user is None:
+        return jsonify({'error': 'Invalid or missing authorization'}), 401
+
     if request.method == 'POST':
         # Get favourite coffee from JSON body
         coffee = request.json.get('favouriteCoffee')
         if not coffee:
             return jsonify({'error': 'Missing favouriteCoffee in JSON body'}), 400
-
-        # Find user in database or create a new one
-        user = User.query.filter_by(username=user_name).first()
-        if user is None:
-            user = User(username=user_name)
-            db.session.add(user)
 
         # Update user's favourite coffee
         user.coffee = coffee
@@ -56,11 +88,8 @@ def favourite_coffee():
 
     elif request.method == 'GET':
         # Return user's favourite coffee
-        user = User.query.filter_by(username=user_name).first()
-        if user is not None:
-            return jsonify({'favouriteCoffee': user.coffee})
-        else:
-            return jsonify({'error': 'User not found'}), 404
+        return jsonify({'favouriteCoffee': user.coffee})
+
 
 @v1_blueprint.route('/admin/coffee/favourite/leadeboard', methods=['GET'])
 def leaderboard():
@@ -72,12 +101,15 @@ def leaderboard():
                              .all()
     return jsonify({'top3': [{coffee: count} for coffee, count in top_coffees]})
 
+
 # Register our blueprint under the url_prefix /v1
 app.register_blueprint(v1_blueprint)
+
 
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify({'status': 'healthy'})
+
 
 if __name__ == "__main__":
     # Create database tables if they don't exist
